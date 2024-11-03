89c2a2971798e83fd4789ee76606e96a931927ae
codebyaadi <adityastar57@gmail.com>
2024-11-03 10:43:52 +0530

    emptied the project with only .gitignore file

--- React/src/api/api.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-const api: AxiosInstance = axios.create({
-    baseURL: "https://fastapi-react-app-backend.onrender.com/",
-    // baseURL: 'http://127.0.0.1:8000/',
-});
-
-export default api;
